912abcf209bf3e0782a7dd8af4a07dc0641d5245
ChenHaolong <1036749218@qq.com>
2022-09-22 11:30:47 +0800

    purgeEverything on cloudflare

--- server/CloudflareServer.go
+++ server/CloudflareServer.go
@@ -14,7 +14,7 @@ const (
 	port = ":50051"
 )
 
-type server struct{
+type server struct {
 	pb.UnimplementedPurgeCloudflareServer
 }
 
@@ -46,10 +46,42 @@ func (s *server) PurgeCloudflare(ctx context.Context, in *pb.PurgeRequestCloudfl
 	fileList := in.GetPurgeList()
 
 	pcr := cloudflare.PurgeCacheRequest{
-		Files:      fileList,
+		Files: fileList,
 	}
 
 	response, err := api.PurgeCache(ctxAPI, id, pcr)
+	if response.Response.Success == true {
+		return &pb.PurgeReplyCloudflare{Result: true}, nil
+	}
+	return &pb.PurgeReplyCloudflare{Result: false}, nil
+}
+
+func (s *server) PurgeCloudflareEverything(ctx context.Context, in *pb.PurgeRequestCloudflareEverything) (*pb.PurgeReplyCloudflare, error) {
+	log.Printf("apiKey Received: %v", in.GetApiKey())
+	log.Printf("apiEmail Received: %v", in.GetApiEmail())
+	log.Printf("zoneName Received: %v", in.GetZoneName())
+
+	// Construct a new API object using a global API key
+	api, err := cloudflare.New(in.GetApiKey(), in.GetApiEmail())
+	// alternatively, you can use a scoped API token
+	// api, err := cloudflare.NewWithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+		return &pb.PurgeReplyCloudflare{Result: false}, err
+	}
+
+	// Most API calls require a Context
+	ctxAPI := context.Background()
+
+	// Fetch the zone ID
+	id, err := api.ZoneIDByName(in.GetZoneName()) // Assuming example.com exists in your Cloudflare account already
+	if err != nil {
+		log.Fatal(err)
+		return &pb.PurgeReplyCloudflare{Result: false}, err
+	}
+	// set purgeRequest
+
+	response, err := api.PurgeEverything(ctxAPI, id)
 	if response.Response.Success == true {
 		return &pb.PurgeReplyCloudflare{Result: true}, nil
 	}
